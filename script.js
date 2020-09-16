a12a72eca1c0ddcc3b6cab5786db2f10fa5dd2d9
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-16 16:53:42 -0700

    Start Game created

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 
+
+const shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
 
 function startGame() {
   console.log('Started')
   startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -16,3 +22,13 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+  {
+    question: 'How to you get an integer without any decimals?',
+    answers: [
+      { text: 'Math.floor', correct: true },
+      {text: 'Math.random', correct: false}
+    ]
+  }
+]
