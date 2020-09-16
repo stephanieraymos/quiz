d81151e5b3b91df58219c476f80a482e0a41dabd
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-16 16:40:52 -0700

    Question container shows after start button pressed.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
   console.log('Started')
+  startButton.classList.add('hide')
+  questionContainerElement.classList.remove('hide')
 }
 
 function setNextQuestion() {
