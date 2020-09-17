dacac5f27618d29cb5bde4230a31acb853f994f6
Stephanie Raymos <stephanieraymos@gmail.com>
2020-09-16 17:40:40 -0700

    Quiz set up and working

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
   startButton.classList.add('hide')
@@ -51,6 +55,12 @@ function selectAnswer(e) {
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
+  if(shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
 }
 
 function setStatusClass(element, correct) {
@@ -69,10 +79,67 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: 'How to you get an integer without any decimals?',
+    question: 'How do you get an integer without any decimals?',
     answers: [
       { text: 'Math.floor', correct: true },
-      { text: 'Math.random', correct: false }
+      { text: 'Math.random', correct: false },
+      { text: 'Math.round', correct: false },
+      { text: 'Math.int', correct: false }
+
+    ]
+  },
+  {
+    question: 'Which of the following is NOT a JavaScript type?',
+    answers: [
+      { text: 'true', correct: true },
+      { text: 'Number', correct: false },
+      { text: 'String', correct: false },
+      { text: 'Boolean', correct: false }
+    ]
+  },
+  {
+    question: 'How do you create a multi line comment in JavaScript?',
+    answers: [
+      { text: '//This is a comment', correct: false },
+      { text: '*/This is a comment/*', correct: false },
+      { text: '/*This is a comment*/', correct: true },
+      { text: '<!--This is a comment-->', correct: false }
+    ]
+  },
+  {
+    question: 'Which of these is NOT a looping structure in Javascript',
+    answers: [
+      { text: 'if', correct: true },
+      { text: 'for', correct: false },
+      { text: 'while', correct: false },
+      { text: 'do-while', correct: false }
+    ]
+  },
+  {
+    question: 'Which of the following is NOT a type of pop up box available in JavaScript?',
+    answers: [
+      { text: 'Alert', correct: false },
+      { text: 'Confirm', correct: false },
+      { text: 'Question', correct: true },
+      { text: 'Prompt', correct: false }
+    ]
+  },
+  {
+    question: 'Which of the following is NOT a JavaScript method?',
+    answers: [
+      { text: 'unshift()', correct: false },
+      { text: 'toUpperCase()', correct: false },
+      { text: 'lastString()', correct: true },
+      { text: 'indexOf()', correct: false }
+    ]
+  },
+  {
+    question: 'Which of the following canNOT be done with client-side JavaScript?',
+    answers: [
+      { text: 'Validating a form', correct: false },
+      { text: 'Storing the forms contents to a database file on the server', correct: true },
+      { text: 'Sending a forms contents by email', correct: false },
+      { text: 'None of the above', correct: false }
     ]
   }
 ]
